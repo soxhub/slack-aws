e2236417757d28d271705f4596cf2ca2d2198905
Kevin Jhangiani <kevin@soxhub.com>
2016-06-07 19:35:36 +0000

    add restoredb and backupdb recipes to bot

--- slack-aws/commands/ops_works.rb
+++ slack-aws/commands/ops_works.rb
@@ -295,7 +295,7 @@ module SlackAws
               fail "Instance *#{hostname}* does not exist.  Use `aws ops instance ls` to see existing instances." unless instance
               
               commands = opsworks_client.describe_commands(instance_id: instance.instance_id).commands
-              fail "another command is currently running.  please wait for the prior command to complete before upgrading.  the prior command is in status *#{commands[0].status}*" if commands.size && commands[0].status != "successful" && commands[0].status != "failed"
+              fail "another command is currently running.  please wait for the prior command to complete before cloning.  the prior command is in status *#{commands[0].status}*" if commands.size && commands[0].status != "successful" && commands[0].status != "failed"
               
               from_stack = arguments.shift
               fail "<from> cannot be empty.  Use syntax `<stack>:<instance>` to specify which instance to clone from." if !from_stack || from_stack.empty?
@@ -306,9 +306,30 @@ module SlackAws
               fail "<from_instance> cannot be empty. Use syntax `<stack>:<instance>` to specify which instance to clone from." if !from_instance || from_instance.empty?
               
               
-              clone_response = opsworks_client.create_deployment(stack_id: @@current_stack_id, instance_ids:[instance.instance_id], command: { name: 'execute_recipes', args: { recipes: ["soxhub::clone_db"] }}, custom_json:"{\"soxhub\": { \"clone_db\": { \"instances\": { \"#{hostname}\": true }, \"from\": { \"stack\": \"#{from_stack}\", \"instance\":\"#{from_instance}\" } }}}")
+              db_response = opsworks_client.create_deployment(stack_id: @@current_stack_id, instance_ids:[instance.instance_id], command: { name: 'execute_recipes', args: { recipes: ["soxhub::clone_db"] }}, custom_json:"{\"soxhub\": { \"clone_db\": { \"instances\": { \"#{hostname}\": true }, \"from\": { \"stack\": \"#{from_stack}\", \"instance\":\"#{from_instance}\" } }}}")
               
               send_message client, data.channel, "CLONING DB FROM: stack: `#{from_stack}`, instance: `#{from_instance}`"
+              send_message client, data.channel, "instance: *#{hostname}*, stack: *#{@@current_stack}*"
+              send_message client, data.channel, "use `aws ops instance status #{hostname}` or login to opsworks to view the status of this operation."
+              
+              
+            when 'restoredb' then
+              hostname = arguments.shift
+              fail 'Invalid instance name.  Use `aws ops instance ls` to see available instances in stack *#{@@current_stack}*.' unless hostname
+              
+              instance_hash = Hash[response.instances.map { |instance| [instance.hostname, instance] }]
+              instance = instance_hash[hostname]
+              fail "Instance *#{hostname}* does not exist.  Use `aws ops instance ls` to see existing instances." unless instance
+              
+              commands = opsworks_client.describe_commands(instance_id: instance.instance_id).commands
+              fail "another command is currently running.  please wait for the prior command to complete before restoring.  the prior command is in status *#{commands[0].status}*" if commands.size && commands[0].status != "successful" && commands[0].status != "failed"
+              
+              sql_file = arguments.shift
+              fail "<sql_file> cannot be empty.  Please specify an sql file that exists on this instance in the `soxhub-database` directory."
+              
+              db_response = opsworks_client.create_deployment(stack_id: @@current_stack_id, instance_ids:[instance.instance_id], command: { name: 'execute_recipes', args: { recipes: ["soxhub::restore_db"] }}, custom_json:"{\"soxhub\": { \"restore_db\": { \"instances\": { \"#{hostname}\": true }, \"sql_file\": \"#{sql_file}\" }}}")
+              
+              send_message client, data.channel, "RESTORING DB FROM FILE: `#{sql_file}`"
               send_message client, data.channel, "instance: *#{hostname}*, stack: *#{@@current_stack}*"
               send_message client, data.channel, "use `aws ops instance status #{hostname}` or login to opsworks to view the status of this operation."
               
@@ -322,11 +343,28 @@ module SlackAws
               fail "Instance *#{hostname}* does not exist.  Use `aws ops instance ls` to see existing instances." unless instance
               
               commands = opsworks_client.describe_commands(instance_id: instance.instance_id).commands
-              fail "another command is currently running.  please wait for the prior command to complete before upgrading.  the prior command is in status *#{commands[0].status}*" if commands.size && commands[0].status != "successful" && commands[0].status != "failed"
+              fail "another command is currently running.  please wait for the prior command to complete before emptying.  the prior command is in status *#{commands[0].status}*" if commands.size && commands[0].status != "successful" && commands[0].status != "failed"
               
-              empty_response = opsworks_client.create_deployment(stack_id: @@current_stack_id, instance_ids:[instance.instance_id], command: { name: 'execute_recipes', args: { recipes: ["soxhub::empty_db"] }}, custom_json:"{\"soxhub\": { \"empty_db\": { \"instances\": { \"#{hostname}\": true } }}}")
+              db_response = opsworks_client.create_deployment(stack_id: @@current_stack_id, instance_ids:[instance.instance_id], command: { name: 'execute_recipes', args: { recipes: ["soxhub::empty_db"] }}, custom_json:"{\"soxhub\": { \"empty_db\": { \"instances\": { \"#{hostname}\": true } }}}")
               
               send_message client, data.channel, "EMPTY DB operation started!"
+              send_message client, data.channel, "instance: *#{hostname}*, stack: *#{@@current_stack}*"
+              send_message client, data.channel, "use `aws ops instance status #{hostname}` or login to opsworks to view the status of this operation."
+              
+            when 'backupdb' then
+              hostname = arguments.shift
+              fail 'Invalid instance name.  Use `aws ops instance ls` to see available instances in stack *#{@@current_stack}*.' unless hostname
+              
+              instance_hash = Hash[response.instances.map { |instance| [instance.hostname, instance] }]
+              instance = instance_hash[hostname]
+              fail "Instance *#{hostname}* does not exist.  Use `aws ops instance ls` to see existing instances." unless instance
+              
+              commands = opsworks_client.describe_commands(instance_id: instance.instance_id).commands
+              fail "another command is currently running.  please wait for the prior command to complete before backing up.  the prior command is in status *#{commands[0].status}*" if commands.size && commands[0].status != "successful" && commands[0].status != "failed"
+              
+              db_response = opsworks_client.create_deployment(stack_id: @@current_stack_id, instance_ids:[instance.instance_id], command: { name: 'execute_recipes', args: { recipes: ["soxhub::backup_db"] }}, custom_json:"{\"soxhub\": { \"backup_db\": { \"instances\": { \"#{hostname}\": true } }}}")
+              
+              send_message client, data.channel, "BACKUP DB operation started!"
               send_message client, data.channel, "instance: *#{hostname}*, stack: *#{@@current_stack}*"
               send_message client, data.channel, "use `aws ops instance status #{hostname}` or login to opsworks to view the status of this operation."
               
@@ -351,7 +389,7 @@ module SlackAws
               send_message client, data.channel, "`aws ops instance <command>`"
               send_message client, data.channel, "instance commands: `ls`, `hosts`, `start <name>`, `stop <name>`, `status <name>`, `create <name> <type|default:t2.small> <availability_zone|default:us-west-2b>`,  `delete <name>`"
               send_message client, data.channel, "provision recipes: `provision <name> <api_branch> <client_branch> <from_stack>:<from_instance>`, `provision <name> <api_branch> <client_branch> empty:empty`, `provisiondev <name> <api_branch|default:live> <client_branch|default:live>`"
-              send_message client, data.channel, "additional recipes: `ucc <name>`, `upgrade <name> <api_branch|default:live> <client_branch|default:live>`, `clonedb <name> <from_stack>:<from_instance>`, `emptydb <name>`, `grantdb <name>`"
+              send_message client, data.channel, "additional recipes: `ucc <name>`, `upgrade <name> <api_branch|default:live> <client_branch|default:live>`, `clonedb <name> <from_stack>:<from_instance>`, `backupdb <name>`, `restoredb <name> <sql_file>`, `emptydb <name>`, `grantdb <name>`"
               send_message client, data.channel, "current stack: *#{@@current_stack}*" 
               
           end
